15838c2c6f997f70b7993c9a3110896ad8944a34
yoluwo <yetunde.oluwo@vogappdevelopers.com>
2022-10-05 09:59:49 -0600

    Added code

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 //Functions
 //Arrow function
+//Ctrl K + Q to launch Quokka
 
 const isOdd2 = () => {console.log(1 +2);}
 isOdd2()
+
+
+//find the shortest word in a sentence
+
+function findShort(s) {
+    var words = s.split(' ');
+    var shortest = words.reduce((shortestWord, curr) => {
+    return curr.length < shortestWord.length ? curr : shortestWord;
+  }, words[0]);
+  return shortest;
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
 
